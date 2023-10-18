7b8c6d512b119e808d176896ec8954a3c0f1dcbe
me2m8 <elmer.brannstrom@gmail.com>
2023-10-18 23:04:00 +0200

    Added steering function and started the PID function

--- main/main.ino
+++ main/main.ino
@@ -6,17 +6,32 @@ struct twoByteSignedChar {
   bool sign;
 };
 
-void setup() {
-  // put your setup code here, to run once:
+// Motor 1 pins, left motor
+const char E1 = 5;
+const char M1 = 4;
+// Motor 2 pins, right motor
+const char E2 = 6;
+const char M2 = 7;
+
+void setup() 
+{
+  // Sets the motor pins to OUTPUT
+  for (int i = 4; i <= 7; i++)
+    pinMode(i, OUTPUT);
+
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   char x = 255;
   analogWrite(13, x);
 }
 
-bool isNegative(short n) {
+bool isNegative(short n) 
+{
     return n >> (sizeof(n) * 8) - 1;
 }
 
@@ -24,3 +39,26 @@ int readArray()
 {
   // TODO
 }
+
+twoByteSignedChar PID(int input)
+{
+  int goal = 0; // Depending on your implementation of readArray(), change this to be the middle value.
+  int error = input - goal;
+
+  // TODO
+}
+
+// Sets the speed of the wheel based oon the input
+void steer(twoByteSignedChar input)
+{
+  if (input.sign) { // If negative
+    // Set right wheel to max and reduce speed on left wheel
+    analogWrite(E1, (input.num ^ 0xFF)); 
+    analogWrite(E2, 0xFF);
+  } else {  // If positive
+    // Set left wheel to max and reduce speed on right wheel
+    analogWrite(E1, 0xFF);
+    analogWrite(E1, (input.num ^ 0xFF));
+  }
+}
+
