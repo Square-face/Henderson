2d8d41bd13a75a462cbb60fc5bb117c96d3f5b70
Square-Face <sq8.square.face@gmail.com>
2023-10-18 20:09:01 +0200

    added fucky wucky struct

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,14 @@
+
+// hacky optimization for stupid shit
+// why use 2's complement when you can use 2 bytes
+struct twoByteSignedChar {
+  char num;
+  bool sign;
+};
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
 }
 
 void loop() {
