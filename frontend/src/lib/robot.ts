221789b0b64b614022a78e7eef66c0a7708f9804
Square-face <linus.michelsson@gmail.com>
2023-11-12 20:19:38 +0100

    basic robot schema

---
+++ frontend/src/lib/robot.ts
@@ -0,0 +1,52 @@
+import { writable, type Writable } from 'svelte/store';
+import { Socket } from './socket';
+import { type Config, defaultConfig } from './types';
+
+
+class Robot {
+  
+  config: Writable<Config>;
+  private socket: Socket;
+
+  constructor() {
+    this.socket = new Socket();
+    this.config = writable(defaultConfig);
+  }
+
+  /**
+   * Get the raw config.
+   *
+   * NOTE: It is adviced to use [Robot.subscribe] as it will update if the config changes
+   * */
+  getConfig(): Config {
+    let current: Config = defaultConfig;
+
+    this.config.subscribe((value) => {
+      current = value
+    });
+
+    return current
+  }
+
+  /**
+   * Upload the local config to the robot
+   *
+   * Everything in thew 
+   * */
+  write() {
+    this.socket.sendConfig(this.getConfig());
+  }
+
+
+  /**
+   * Request the robot to send its current config values.
+   *
+   * This method does not return anything by itself. Instead it will trigger a event when the server gets a response from the robot with the new values and will subsequently replace the saved local config with the one supplised by the server
+   * */
+  read() {
+    this.socket.requestConfig();
+  }
+}
+
+
+export const Henderson = new Robot()
---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,41 @@
+
+// All possible states
+export enum State {
+  STANDBY = "Standby",
+  RUNNING = "Running",
+  CALIBRATING = "Calibrating",
+  CALIBRATING_MANUAL = "Manual calibration",
+}
+
+// interface for the data type holding all values that can be sent to the server to modify Henderson
+export interface Config {
+  State: State,
+  Pk: number,
+  Ik: number,
+  Dk: number,
+  Speed: number
+}
+
+export interface ConfigMask {
+  State: boolean,
+  Pk: boolean,
+  Ik: boolean,
+  Dk: boolean,
+  Speed: boolean,
+}
+
+export const defaultConfig: Config = {
+  State: State.STANDBY,
+  Pk: 0,
+  Ik: 0,
+  Dk: 0,
+  Speed: 0,
+}
+
+export const MaskOn: ConfigMask = {
+  State: true,
+  Pk: true,
+  Ik: true,
+  Dk: true,
+  Speed: true,
+}
