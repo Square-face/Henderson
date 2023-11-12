6b743ee08baa338de4aea63f0da337e262a23999
Square-Face <sq8.square.face@gmail.com>
2023-11-12 18:36:36 +0100

    confusus once said

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    host: "127.0.0.1"
-  }
 })
