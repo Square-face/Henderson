8be28a49b3eab163662306934a096be15b39e5f8
me2m8 <elmer.brannstrom@gmail.com>
2023-11-12 20:22:29 +0100

    start of UI

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,46 +2,50 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Slider from './lib/Slider.svelte';
+
+  let Pk = 0;
+  let Ik = 0;
+  let Dk = 0;  
+  let speed = 0;
+
+  function handleClick() {
+    // Send the thing
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="appContainer">
+    <div class="misc">
+      <h1>Misc</h1>
+      <span>Request</span>
+      <input type="checkbox" />
+
+      <button type="button" on:click={handleClick}>SEND</button>
+    </div>
+    <div class="PID">
+      <h1>PID</h1>
+      <Slider bind:value={Pk} text="P" max={0.02}/>
+      <Slider bind:value={Ik} text="I" max={0.02}/>
+      <Slider bind:value={Dk} text="D" max={0.02}/>
+    </div>
+    <div class="speed">
+      <h1>Speed</h1>
+      <Slider bind:value={speed} text="Speed" max={255}/>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  button {
+    margin-left: 10rem;
+    height: 5rem;
+    width: 10rem;
+    background-color: cyan;
+    color: #242424;
+    border: none; 
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  button:hover{
+    background-color: rgb(0, 228, 228);
   }
 </style>
---
+++ frontend/src/lib/Slider.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let max: number;
+  export let value = 0;
+  export let checked = false;
+  export let text = "default text"
+</script>
+
+<div>
+  <input type="number" max={max} step={(max < 1) ? max / 10000 : 1} class="slider-text" bind:value/>
+  <input type="range" max={max} step={(max < 1) ? max / 10000 : 1} bind:value>
+  <input type="number" class="slider-text" bind:value={max}/>
+  <div class="span-check-block">
+    <span>{text}</span>
+    <input type="checkbox" bind:checked/>
+  </div>
+</div>
+
+<style>
+  .slider-text {
+    width: 10vw;
+    appearance: none;
+    border: none;
+    border-bottom: 3px solid cyan;
+    outline: none;
+    border-radius: .5em .5em .1em .1em;
+    background-color: #242424;
+    color: #ccc;
+    position: relative;
+    bottom: 4px;
+  }
+
+  .span-check-block {
+    width: 5vw;
+    display: inline-flex;
+    justify-content: space-between;
+  }
+</style>
