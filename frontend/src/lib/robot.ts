3cfcc862a6c59f95739491f4835472ff787dd300
Square-face <linus.michelsson@gmail.com>
2023-11-12 21:25:56 +0100

    fixed bad function name

--- frontend/src/lib/robot.ts
+++ frontend/src/lib/robot.ts
@@ -47,7 +47,7 @@ class Robot {
    *
    * This method does not return anything by itself. Instead it will trigger a event when the server gets a response from the robot with the new values and will subsequently replace the saved local config with the one supplised by the server
    * */
-  read() {
+  what_the_hell_are_you_doing() {
     this.socket.requestConfig();
   }
 }
