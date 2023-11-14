788bedee2d929e28c2ae3588cce408879447adc7
Square-Face <sq8.square.face@gmail.com>
2023-11-14 02:22:23 +0100

    things

--- controller/main.py
+++ controller/main.py
@@ -12,27 +12,26 @@ manager = ConnectionManager()
 buffer = bytearray()
 
 async def callback(_: BleakGATTCharacteristic, data: bytearray):
-    print(data)
     global buffer
+    print("BLE callback")
     
     start_length = len(buffer)
 
     buffer += data
 
+    print(f"bl({len(buffer)}) < {buffer.hex()}")
+
     if start_length == 0 and len(buffer) == 14:
-        print(f"bl({len(buffer)}) < {buffer.hex()}")
         await manager.broadcast(decode_status(buffer))
         buffer.clear()
     
     # clear buffer if it was 0 before and its less than a full block now
     # it means the block just sent will not be followed by anything related as it is not a full block
     if start_length == 0 and len(buffer) != 20:
-        print(f"bl({len(buffer)}) < {buffer.hex()}")
         buffer.clear()
         return
 
     if start_length != 0 and len(buffer) == 38:
-        print(f"bl({len(buffer)}) < {buffer.hex()}")
         await manager.broadcast(decode_log(buffer))
         buffer.clear()
         return
